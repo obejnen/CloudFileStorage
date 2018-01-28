e3909364c47cc54a7e0e9f2f972fe72181a64eb9
obejnen <obejnen@gmail.com>
2018-01-28 17:45:41 +0300

    mulptiple file upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,24 @@
 class ItemsController < ApplicationController
+    before_action :parse_files, only: [:create]
     before_action :set_folder, only: [:create]
     
     def create
-        Item.create(item_params)
+        files = params.require(:item).permit(file: [])[:file]
+        files.each do |file|
+            Item.create(name: file.original_filename, user_id: current_user.id, folder_id: @folder.id, file: file)
+        end
     end
 
     private
 
+    def parse_files
+        files = params.require(:item).permit(:file)
+    end
+
     def item_params
-        @name = params.require(:item).require(:file).original_filename
+        # @name = params.require(:item).require(:file).original_filename
         params.require(:item).permit(:file).merge(user_id: current_user.id, folder_id: @folder.id, name: @name)
+        raise "exc"
     end
 
     def set_folder
